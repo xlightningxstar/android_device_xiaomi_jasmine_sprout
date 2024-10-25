2f7901812d6d68d7d3b35f0a8163fed33d557d7b
⚡ŞŦΔŘŁØŘĐ <soumodip.dgp@gmail.com>
2024-10-25 12:31:41 +0530

    Update lineage_jasmine_sprout.mk

--- lineage_jasmine_sprout.mk
+++ lineage_jasmine_sprout.mk
@@ -1,6 +1,5 @@
 #
-# Copyright (C) 2018-2021 The LineageOS Project
-# Copyright (C) 2021 Pixel Experience
+# Copyright (C) 2022 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,28 +10,19 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 
-# Inherit some common ArrowOS stuff
-$(call inherit-product, vendor/arrow/config/common.mk)
-
-# ArrowOS Maintainer Config
-DEVICE_MAINTAINER := Immanuel_Raj(@iamimmanuelraj)
-
-# We are a phone
-IS_PHONE := true
-
-# Gapps Architecture
-TARGET_GAPPS_ARCH := arm64
-
-# Boot Aniamation Resolution
-TARGET_BOOT_ANIMATION_RES := 1080
-
 # Inherit from jasmine_sprout device
 $(call inherit-product, $(LOCAL_PATH)/device.mk)
 
+# Inherit some common Lineage stuff
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+
+# Device identifier. This must come after all inclusions.
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := jasmine_sprout
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Mi A2
 PRODUCT_NAME := arrow_jasmine_sprout
+
+PRODUCT_CHARACTERISTICS := nosdcard
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
