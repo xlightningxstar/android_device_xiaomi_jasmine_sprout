71bf2f269bb29ee6ad21bb0206ca157cedab8eb5
⚡ŞŦΔŘŁØŘĐ <soumodip.dgp@gmail.com>
2026-03-29 10:17:03 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -6,9 +6,8 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifeq ($(TARGET_DEVICE),lavender)
-  subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
-  $(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
-	
+ifeq ($(TARGET_DEVICE),jasmine_sprout)
+subdir_makefiles=$(call first-makefiles-under,$(LOCAL_PATH))
+$(foreach mk,$(subdir_makefiles),$(info including $(mk) ...)$(eval include $(mk)))
 include $(CLEAR_VARS)
 endif
