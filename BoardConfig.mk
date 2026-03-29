32159ac076119eb8873f8049b416aa1a90f93dbf
⚡ŞŦΔŘŁØŘĐ <soumodip.dgp@gmail.com>
2026-03-29 09:33:52 +0530

    Update BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,11 +1,15 @@
 #
 # Copyright (C) 2018 The LineageOS Project
-# Copyright (C) 2018-2021 Xioami-SDM660 Project
+# Copyright (C) 2018-2021 Xiaomi-SDM660 Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Inherit from sdm660-common
 include device/xiaomi/sdm660-common/BoardConfigCommon.mk
+
+# Device Path
+DEVICE_PATH := device/xiaomi/jasmine_sprout
 
 # AVB
 BOARD_AVB_ENABLE := true
@@ -15,9 +19,6 @@ BOARD_AVB_RECOVERY_ROLLBACK_INDEX := $(PLATFORM_SECURITY_PATCH_TIMESTAMP)
 BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
 BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
 
-# Audio
-AUDIO_FEATURE_ELLIPTIC_ULTRASOUND_SUPPORT := true
-
 # Camera
 TARGET_TS_MAKEUP := true
 TARGET_USES_QTI_CAMERA_DEVICE := true
@@ -25,27 +26,29 @@ BOARD_QTI_CAMERA_32BIT_ONLY := true
 USE_DEVICE_SPECIFIC_CAMERA := true
 $(call project-set-path,qcom-camera,$(DEVICE_PATH)/camera)
 
-# Device Path
-DEVICE_PATH := device/xiaomi/lavender
-
-# DT2W
-TARGET_TAP_TO_WAKE_NODE := "/sys/touchpanel/double_tap"
-
 # Density
-TARGET_SCREEN_DENSITY := 420
+TARGET_SCREEN_DENSITY := 440
 
 # Kernel
-TARGET_KERNEL_CONFIG += vendor/xiaomi/lavender.config
- 
+TARGET_KERNEL_CONFIG += vendor/xiaomi/jasmine_sprout.config
+
 # Manifest
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/framework_compatibility_matrix.xml
 
-# Partitions
-BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE := 3758096384
+# Partitions — Retrofitted dynamic for jasmine (no cust partition)
+BOARD_SUPER_PARTITION_BLOCK_DEVICES := system vendor
+BOARD_SUPER_PARTITION_METADATA_DEVICE := system
+BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE := 3221225472
 BOARD_SUPER_PARTITION_VENDOR_DEVICE_SIZE := 2147483648
-BOARD_SUPER_PARTITION_SIZE := $(shell expr $(BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE) + $(BOARD_SUPER_PARTITION_VENDOR_DEVICE_SIZE) + $(BOARD_SUPER_PARTITION_CUST_DEVICE_SIZE))
-BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := $(shell expr $(BOARD_SUPER_PARTITION_SIZE) - 4194304 )
+BOARD_SUPER_PARTITION_SIZE := $(shell expr \
+    $(BOARD_SUPER_PARTITION_SYSTEM_DEVICE_SIZE) + \
+    $(BOARD_SUPER_PARTITION_VENDOR_DEVICE_SIZE))
+BOARD_XIAOMI_DYNAMIC_PARTITIONS_SIZE := $(shell expr \
+    $(BOARD_SUPER_PARTITION_SIZE) - 4194304)
 
-# WLAN MAC
-WLAN_MAC_SYMLINK := true
+# Recovery
+TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
+
+# Inherit the proprietary files
+include vendor/xiaomi/jasmine_sprout/BoardConfigVendor.mk
